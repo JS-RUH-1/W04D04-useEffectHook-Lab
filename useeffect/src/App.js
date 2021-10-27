33ef1cdba490bbfef329cc0796ed9c51999947ed
TheRealBasel <alanaziibasel@gmail.com>
2021-10-27 11:29:43 +0300

    Finish Lab

---
+++ useeffect/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(
+    () => {
+      window.addEventListener('resize', () => setWidth(window.innerWidth))
+    }
+  )
+  return ( <h1>{width}</h1> );
+}
+
+export default App;
